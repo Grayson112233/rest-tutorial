06e8f5b9f5ea4842bb5fd26450e3fe99f33a30a4
Grayson Pike <graysonpike@gmail.com>
2015-06-09 11:31:40 -0500

    Added users and ownership of SNippets

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,26 +1,16 @@
 from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from django.contrib.auth.models import User
 
 # Method to serialize snippet objects to a dictionary
-class SnippetSerializer(serializers.Serializer):
-	pk = serializers.IntegerField(read_only=True) # Primary Key, should not be changed
-	title = serializers.CharField(required=False, allow_blank=True, max_length=100) # Name/Title for snippet, optional
-	code = serializers.CharField(style={'base_template': 'textarea.html'}) # Field to enter code, uses textarea when rendered to HTML
-	linenos = serializers.BooleanField(required=False) # Line Numbers Toggle
-	language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python') # Lang being colored
-	style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly') # Style for code coloring
+class SnippetSerializer(serializers.ModelSerializer):
+	owner = serializers.ReadOnlyField(source='owner.username')
+	class Meta:
+		model = Snippet
+		fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
 
-	def create(self, validated_data):
-		# Create and return a new Snippet instance given valid data
-		return Snippet.objects.create(**validated_data) # ** means we are passing a dictionary (validated_data)
-
-	def update(self, instance, validated_data):
-		# Update and return Snippet instance given new validated_data
-		instance.title = validated_data.get('title', instance.title)
-		instance.code = validated_data.get('code', instance.code)
-		instance.linenos = validated_data.get('linenos', instance.linenos)
-		instance.language = validated_data.get('language', instance.language)
-		instance.style = validated_data.get('style', instance.style)
-		instance.save()
-		return instance
+class UserSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = User
+		fields = ('id', 'username', 'snippets')
